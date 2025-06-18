518134c891cb860c482b5a0c6d8b5a680ff6fed3
Henrique Siqueira Lima <henrynyyls@gmail.com>
2025-06-18 15:10:01 -0300

    feat: add new unit test and also configured the E2E test

--- src/tests/userInfoUtils.test.ts
+++ src/tests/userInfoUtils.test.ts
@@ -2,8 +2,14 @@ import getUserInformation from "../utils/userInfoUtils";
 
 describe("getUserInformation", () => {
   it("should return user information", async () => {
-    const userId = 11;
+    const userId = 1;
     const userInformation = await getUserInformation(userId);
     expect(userInformation).toBeDefined();
+  });
+
+  it("should not return user information because of wrong user ID", async () => {
+    const userId = 999999;
+    const userInformation = await getUserInformation(userId);
+    expect(userInformation).toBeUndefined();
   });
 });
