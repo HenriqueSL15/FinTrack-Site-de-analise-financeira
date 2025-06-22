aef69aae82449cd9dfdbfd21e53f985b302b409b
Henrique Siqueira Lima <henrynyyls@gmail.com>
2025-06-22 20:22:21 -0300

    refactor: refactor category select trigger and item IDs
    Moved the 'selectCategory' id from SelectValue to SelectTrigger and updated SelectItem ids to be unique per category. This improves accessibility and ensures unique element identifiers.

--- src/components/layout/Budget/NewBudgetDialog.tsx
+++ src/components/layout/Budget/NewBudgetDialog.tsx
@@ -127,11 +127,8 @@ function NewBudgetDialog() {
                     defaultValue={field.value}
                   >
                     <FormControl className="w-full">
-                      <SelectTrigger>
-                        <SelectValue
-                          placeholder="Selecione uma categoria"
-                          id="selectCategory"
-                        />
+                      <SelectTrigger id="selectCategory">
+                        <SelectValue placeholder="Selecione uma categoria" />
                       </SelectTrigger>
                     </FormControl>
                     <SelectContent>
@@ -141,7 +138,7 @@ function NewBudgetDialog() {
                           <SelectItem
                             key={category.id}
                             value={category.name}
-                            id="categoryItem"
+                            id={`categoryItem-${category.name}`}
                           >
                             {category.name}
                           </SelectItem>
